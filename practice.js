f2b56b36ef7e59a5aa8279386b245cfadb91df52
najim77288 <najimuddin77288@gmail.com>
2024-02-02 17:12:37 +0600

    for did umcomment

--- practice.js
+++ practice.js
@@ -1,58 +1,36 @@
           //>>>>>>>>>>>> Practice tasks<<<<<<<<<<<//
+           // >>>>>>>>>>>> <<<<<<<<<<<<<<
+        var fruit=['banana', 'orange', 'mango', 'lemon'] //array
+        var reversed = [];
 
-        // const myFriends = ["Najim", "Helal", "Mohin", "Bodrul", "Emad", "Ahad"]
-        // const reversed = [];
+        for(i = fruit.length -1; i > -1; i--) {
+          reversed.push(fruit[i])
+            }
+           console.log(reversed)
+          console.log(evenNumbers);
+          
+          
+                // >>>>>>>>>>>> <<<<<<<<<<<<<<
+          var numbersArray = [12, 98, 5, 41, 23, 78, 46]
+          evenNumbers = [];
 
-        // for(i = myFriends.length -1; i > -1; i--){
-        //     reversed.push(myFriends[i])
-        // }
-        // console.log(reversed)
+          for(let i = 0; i < numbersArray.length; i++) {
+            if(numbersArray[i] % 2 === 0) {
+              evenNumbers.push(numbersArray[i])
+            }
+          }
+          console.log(evenNumbers)
 
-    //    const books = ['hadith', 'quran', 'fiqho', 'arbic', 'bangla']
-    //    books.reverse()
-    //    console.log(books)
+                // >>>>>>>>>>>> <<<<<<<<<<<<<<
+          const najimNumbers = [20,50,55,11,33,44,60]
+          const najimEven = []
 
-    // var fruit=['banana', 'orange', 'mango', 'lemon'] //array
-
-    // var reversed = [];
-
-    // for(i = fruit.length -1; i > -1; i--) {
-    //   reversed.push(fruit[i])
-    // }
-    // console.log(reversed)
-
-    // var numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    // var evenNumbers = [];
-
-    // for (var i = 0; i < numbersArray.length; i++) {
-    //     if (numbersArray[i] % 2 === 0) {
-    //         evenNumbers.push(numbersArray[i]);
-    //     }
-    // }
-
-    // console.log(evenNumbers);  // Output: [2, 4, 6, 8, 10]
-
-
-    // var numbersArray = [12, 98, 5, 41, 23, 78, 46]
-    // evenNumbers = [];
-
-    // for(let i = 0; i < numbersArray.length; i++) {
-    //   if(numbersArray[i] % 2 === 0) {
-    //     evenNumbers.push(numbersArray[i])
-    //   }
-    // }
-    // console.log(evenNumbers)
-
-
-    // const najimNumbers = [20,50,55,11,33,44,60]
-    // const najimEven = []
-
-    // for (let i = 0; i < najimNumbers.length; i++) {
-    //   if (najimNumbers[i] % 2 === 0) {
-    //     najimEven.push(najimNumbers[i])
-    //   }
-    // }
-    // console.log(najimEven)
+          for (let i = 0; i < najimNumbers.length; i++) {
+            if (najimNumbers[i] % 2 === 0) {
+              najimEven.push(najimNumbers[i])
+            }
+          }
+          console.log(najimEven)
 
 
 
@@ -62,11 +40,12 @@
         console.log(text)
           // Output applebananajackfruitelemonwatermelon
 
+           // >>>>>>>>>>>> One <<<<<<<<<<<<<<
         let stringSentence = "Najim is a hard working person He allwayes tryes to learn something";
         let reversed = stringSentence.split(" ").reverse().join(' ')
         console.log(reversed)
 
-
+           // >>>>>>>>>>>> One <<<<<<<<<<<<<
         const najim = "Najim uddin is hafiz and wev developer";
         const my_rev = najim.split(" ").reverse().join(" ");
         console.log(my_rev)
